2b3d88592ad44d7c875799b1a405b224b2c62bae
Kyle <mayogohan1@gmail.com>
2024-01-30 21:33:37 -0800

    sad

--- Assets/thirdPersonTest/scripts/MoveCamera.cs
+++ Assets/thirdPersonTest/scripts/MoveCamera.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class MoveCamera : MonoBehaviour
 {
     public Transform cameraPosition;
+
+    private void Start()
+    {
+        transform.position = cameraPosition.position;
+    }
     private void Update()
     {
         transform.position = cameraPosition.position;
